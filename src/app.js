46c0aabd6d1968aed38f8c5d09e296a8fdf2578c
Suhail Mumtaz <suhailmumtaz097@gmail.com>
2024-07-18 00:14:14 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import Header from  "./component/Header.js";
 import  Body from "./component/Body.js";
 import About from "./component/About.js";
 import Contact from "./component/Contact.js";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 
 
 const Applayout = ()=>{
     return <div className = "app">
   <Header />
-  <Body/>
+  <Outlet/>
     </div> 
   
 }
@@ -18,15 +18,22 @@ const appRouter = createBrowserRouter([
   {
     path:"/",
     element:<Applayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>, 
+      },
+      {
+        path:"/about",
+        element:<About/>,
+      },
+    {
+      path:"/contact",
+      element:<Contact/>,
+    },
+    ],
   },
-  {
-    path:"/about",
-    element:<About/>,
-  },
-{
-  path:"/contact",
-  element:<Contact/>,
-},
+
 ]);
     const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(< RouterProvider router={appRouter} />);
